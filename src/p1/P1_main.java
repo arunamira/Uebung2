12f3536e8e827adffa35c312fce4b1c6a462f892
Aruna Anhuf <a.anhuf@mail.com>
2025-10-30 15:07:09 +0100

    Aufgabe 2

--- src/p1/P1_main.java
+++ src/p1/P1_main.java
@@ -2,7 +2,7 @@ package p1;
 
 import java.util.Scanner;
 
-public class P1_main { 
+public class P1_main {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		String zeile = new String();
