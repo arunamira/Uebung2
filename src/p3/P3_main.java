ac7d72d742106516fdcf2b1b0e18a3a5f022cd53
Aruna Anhuf <a.anhuf@mail.com>
2025-10-30 15:17:37 +0100

    Aufgabe 3

--- src/p3/P3_main.java
+++ src/p3/P3_main.java
@@ -1,5 +1,18 @@
 package p3;
 
 public class P3_main {
+	public static void main(String[] args) {
+		int i = 59, j = 7, k = 2;
+
+		if (i < j) {
+			System.out.println("A");
+		}
+		if (i < k) {
+			System.out.println("B");
+		}
+		if (j < k) {
+			System.out.println("C");
+		}
+	}
 
 }
